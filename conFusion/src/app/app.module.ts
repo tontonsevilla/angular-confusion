1d7cb8384ccbfca0ad4aa8bc875069d2c0f31890
Winston Sevilla <wsevilla@blastasia.com>
2020-09-30 21:42:51 +0800

    Header and Footer

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -14,12 +14,16 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
