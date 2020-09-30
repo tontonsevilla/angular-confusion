e568850457b24610d4fc14cb68dd69910358b2af
Winston Sevilla <wsevilla@blastasia.com>
2020-09-30 22:56:43 +0800

    Assignment 2

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
 
 @NgModule({
   declarations: [
@@ -45,7 +46,8 @@ import { PromotionService } from './services/promotion.service';
     MatButtonModule
   ],
   providers: [
-    PromotionService
+    PromotionService,
+    LeaderService
   ],
   bootstrap: [AppComponent]
 })
