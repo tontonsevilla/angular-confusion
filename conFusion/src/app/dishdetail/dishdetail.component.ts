67bd3203ca5de8d694f9507b1a1baacc9b6f11b3
Winston Sevilla <wsevilla@blastasia.com>
2020-10-01 23:11:34 +0800

    Saving Changes to Server

--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -43,8 +43,10 @@ export class DishdetailComponent implements OnInit {
 
   errMess: string;
 
+  dishcopy: Dish;
+
   constructor(
-    private dishservice: DishService,
+    private dishService: DishService,
     private route: ActivatedRoute,
     private location: Location,
     private fb: FormBuilder,
@@ -54,10 +56,13 @@ export class DishdetailComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.dishservice.getDishIds()
+    this.dishService.getDishIds()
       .subscribe(dishIds => this.dishIds = dishIds, errmess => this.errMess = errmess as any);
-    this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params.id)))
-    .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); }, errmess => this.errMess = errmess as any);
+
+    this.route.params
+      .pipe(switchMap((params: Params) => this.dishService.getDish(params.id)))
+      .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
+        errmess => this.errMess = errmess as any );
   }
 
   setPrevNext(dishId: string) {
@@ -108,7 +113,12 @@ export class DishdetailComponent implements OnInit {
     this.comment.date = new Date().toISOString();
     console.log(this.comment);
 
-    this.dish.comments.push(this.comment);
+    this.dishcopy.comments.push(this.comment);
+    this.dishService.putDish(this.dishcopy)
+      .subscribe(dish => {
+        this.dish = dish; this.dishcopy = dish;
+      },
+      errmess => { this.dish = null; this.dishcopy = null; this.errMess = errmess as any; });
 
     this.formReset();
   }
